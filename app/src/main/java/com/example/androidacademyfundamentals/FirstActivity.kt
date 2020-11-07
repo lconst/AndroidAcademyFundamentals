334c8dfc9288f0e2f275036725cee92947e04c11
e.zaharova <zakh14@bk.ru>
2020-11-07 16:09:46 +0300

    add missed first activity

---
+++ app/src/main/java/com/example/androidacademyfundamentals/FirstActivity.kt
@@ -0,0 +1,50 @@
+package com.example.androidacademyfundamentals
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.*
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+
+class FirstActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "FirstActivity"
+    }
+
+    private var contacts = mutableListOf<String>()
+    private var adapter: ArrayAdapter<String>? = null
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            // Handle the Intent
+            contacts.addAll(intent?.getStringArrayListExtra("contacts")!!.toMutableList())
+            adapter?.notifyDataSetChanged();
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+        val listView = findViewById<ListView>(R.id.list)
+
+        adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1, contacts
+        )
+        listView.setAdapter(adapter)
+
+        val startButton: Button = findViewById(R.id.start)
+
+        startButton.setOnClickListener {
+            contacts.clear()
+            startForResult.launch(Intent(this, SecondActivity::class.java))
+        }
+    }
+
+}
