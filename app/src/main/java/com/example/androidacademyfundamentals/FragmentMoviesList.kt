a9a122cf972472b17c8ecf8996973292dc09325d
e.zaharova <zakh14@bk.ru>
2020-11-24 23:08:37 +0300

    created fragments movies detail and list started to write xml layout for list fragment

---
+++ app/src/main/java/com/example/androidacademyfundamentals/FragmentMoviesList.kt
@@ -0,0 +1,20 @@
+package com.example.androidacademyfundamentals
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+    }
+
+    companion object {
+        fun newInstance() : FragmentMoviesList {
+            return FragmentMoviesList()
+        }
+    }
+}
--- app/src/main/java/com/example/androidacademyfundamentals/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/androidacademyfundamentals/MovieDetailsActivity.kt
@@ -7,5 +7,9 @@ class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
+        supportFragmentManager.beginTransaction()
+                .add(R.id.container, FragmentMoviesDetails.newInstance())
+                .commit()
     }
 }
